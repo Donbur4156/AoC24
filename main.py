8f0bd2b39322a0687c9d63489ff33456763f91de
Donbur4156 <janpfister@t-online.de>
2024-12-05 19:00:32 +0100

    ref main
    generate day by first time execute a day

--- main.py
+++ main.py
@@ -11,26 +11,21 @@ from colorama import init as colorama_init, Fore
 colorama_init(autoreset=True)
 today = datetime.datetime.today()
 
-if len(sys.argv) == 1:
-    if today.year != 2024 or today.month != 12:
-        print(f'{Fore.RED}Error: Can\'t run without specific day outside of competition time')
-        exit(1)
-    target_day = str(datetime.datetime.today().day)
-else:
-    target_day = sys.argv[1]
-
-if target_day == "gen":
-    target_day = str(today.day).zfill(2)
-    if Path(f"day/{target_day}.py").exists():
-        print(f'{Fore.RED}Error: Can\'t generate, current day already exists.')
-        exit(1)
+def get_target_day():
+    if len(sys.argv) == 1:
+        if today.year != 2024 or today.month != 12:
+            print(f'{Fore.RED}Error: Can\'t run without specific day outside of competition time')
+            exit(1)
+        return str(datetime.datetime.today().day)
+    else:
+        return sys.argv[1]
+    
+def generate_day(day: str = None):
+    target_day = day or str(today.day).zfill(2)
     shutil.copyfile('template/day.py', f'aoc2024/days/day{target_day}.py')
     print(f'{Fore.GREEN}Generated template for day {int(target_day)}.')
-else:
-    if int(target_day) > datetime.datetime.today().day:
-        print(f'{Fore.RED}Error: Can\'t run future day')
-        exit(1)
-    target_day = target_day.zfill(2)
+
+def run_day(target_day: str):
     day_data = get_day_data(target_day)
     day_module = import_module(f'aoc2024.days.day{target_day}')
     start_time = perf_counter()
@@ -38,3 +33,9 @@ else:
     end_time = perf_counter() - start_time
     print_result(*results)
     print(f'{Fore.LIGHTBLACK_EX}Perf: Took {end_time:.3f} seconds')
+
+if __name__ == "__main__":
+    target_day = get_target_day().zfill(2)
+    if not Path(f'aoc2024/days/day{target_day}.py').exists():
+        generate_day(target_day)
+    run_day(target_day)
